6aa2bda56da38cfeed293336168da4117ad0df8f
Maksim Litvyakov <litvykov10@mail.ru>
2019-09-15 23:43:52 +0700

    Update cookie.js

--- src/cookie.js
+++ src/cookie.js
@@ -105,7 +105,16 @@ function addCookie(name, value) {
   }
 }
 function deleteCookie(name) {
-  document.cookie = `${name}=''; expires='Thu, 01 Jan 1970 00:00:01 GMT'`;
+  document.cookie = `${name}='';`;
+
+  const cookies = document.cookie.split(";");
+
+  for (let i = 0; i < cookies.length; i++) {
+    let cookie = cookies[i];
+    let eqPos = cookie.indexOf("=");
+    let name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+    document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+  }
 }
 
 
@@ -157,6 +166,7 @@ function isMatching(full, chunk) {
     return false;
   }
 }
+
 function filterObj(value, obj) {
   let filterObj = {};
 
